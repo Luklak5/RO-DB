4e48f187763ad85075c3615680738e0b5ac6103a
Lukas Laubert <l.laubert@seznam.cz>
2024-03-08 12:07:10 +0100

    aaaa

--- index.php
+++ index.php
@@ -2,9 +2,19 @@
 <html>
 <head>
     <meta charset="UTF-8" />
-    <title>title</title>
+    <link rel="stylesheet" href="css/styles.css">
+    <title>RO Database</title>
 </head>
 <body>
+
+    <?php include "includes/nav.php" ?>
+    <?php include "includes/header.php" ?>
+
+    <main>
+        Database
+    </main>
+
+    <?php include "includes/footer.php" ?>
     
 </body>
 </html>
